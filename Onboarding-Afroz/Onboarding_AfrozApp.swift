07940360b14ad76743cbc5b48b86a8f72a2a37af
Brian McIntosh <brian@mixinteractive.com>
2023-05-04 11:44:35 -0400

    Initial Commit

---
+++ Onboarding-Afroz/Onboarding_AfrozApp.swift
@@ -0,0 +1,17 @@
+//
+//  Onboarding_AfrozApp.swift
+//  Onboarding-Afroz
+//
+//  Created by Brian McIntosh on 5/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Onboarding_AfrozApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
